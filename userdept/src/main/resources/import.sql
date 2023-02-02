cce2162dd5ba3452fe14c6e220b24d9f9c31eff7
André <gpvandrequeiroz@gmail.com>
2023-02-02 16:19:40 -0300

    Inclusão de Departamento e novos usuários

--- userdept/src/main/resources/import.sql
+++ userdept/src/main/resources/import.sql
@@ -1,7 +1,10 @@
 INSERT INTO tb_department(name) VALUES ('Gestão');
 INSERT INTO tb_department(name) VALUES ('Informática');
+INSERT INTO tb_department(name) VALUES ('Vendas');
 
 INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Maria', 'maria@gmail.com');
 INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Bob', 'bob@gmail.com');
 INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Alex', 'alex@gmail.com');
 INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (3, 'Arthur', 'Arthur@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (3, 'Queiroz', 'queiroz@gmail.com');
